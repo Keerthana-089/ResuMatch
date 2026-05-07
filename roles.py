5366ccc36039040f6a741a9345c9c638608e855d
Keerthana <c.keerthana089@gmail.com>
2026-05-07 12:11:51 +0530

    Add files via upload

---
+++ roles.py
@@ -0,0 +1,55 @@
+ROLE_KEYWORDS = {
+
+    "Software Engineer": [
+        "python",
+        "javascript",
+        "react",
+        "node.js",
+        "docker",
+        "aws",
+        "rest",
+        "graphql",
+        "kubernetes"
+    ],
+
+    "Data Analyst": [
+        "sql",
+        "excel",
+        "power bi",
+        "tableau",
+        "python",
+        "statistics",
+        "data visualization",
+        "pandas"
+    ],
+
+    "Data Scientist": [
+        "machine learning",
+        "python",
+        "deep learning",
+        "tensorflow",
+        "pandas",
+        "numpy",
+        "nlp",
+        "statistics"
+    ],
+
+    "DevOps Engineer": [
+        "docker",
+        "kubernetes",
+        "jenkins",
+        "aws",
+        "terraform",
+        "linux",
+        "ci/cd"
+    ],
+
+    "Product Manager": [
+        "agile",
+        "roadmap",
+        "jira",
+        "stakeholders",
+        "analytics",
+        "scrum"
+    ]
+}
